ba086b6ff305c88b224d9d228d80bdc85b34ac7f
jkimdev <jkim.dev@icloud.com>
2026-07-11 22:32:27 +0900

    Initial Commit

---
+++ xelkaTests/xelkaTests.swift
@@ -0,0 +1,16 @@
+//
+//  xelkaTests.swift
+//  xelkaTests
+//
+//  Created by jimmy on 7/11/26.
+//
+
+import Testing
+
+struct xelkaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
